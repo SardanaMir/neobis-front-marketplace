86351d1cda7407d640d369f660e93853fe3b3f68
SardanaMir <sardanadord@yandex.ru>
2023-11-20 14:39:52 +0300

    Create CardItem

--- src/MyStore.jsx
+++ src/MyStore.jsx
@@ -11,23 +11,6 @@ function MyStore() {
         setSuccess(true);
     }
   return (
-
-    <>
-        {/* карточка товара, модальное окно */}
-        <div className={success ? 'fixed w-full	h-full top-0 bg-black bg-opacity-50' : 'hidden'}>
-            <div className='w-96 min-h-fit bg-white p-5 rounded-3xl flex flex-col top-2/4 left-2/4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-                <img className="" src="src/assets/img/image 3.jpg" alt="" />
-                <p className='text-2xl text-indigo-600 font-semibold mt-4'>12000</p>
-                <p>Like</p>
-                <p className='text-lg font-semibold mt-4'>Adidas Yeezy 500</p>
-                <p className='text-sm text-gray-400 mt-4'>The Yeezy 500 Blush is a limited edition shoe designed by Kanye West for Adidas</p>
-                <p className='text-base font-semibold mt-4'>Детальная информация</p>
-                <p className='text-sm text-gray-400'>
-                It features a unique design, with a chunky silhouette and a blush colorway. The shoe has a mix of suede, mesh and leather, and it's considered a highly sought-after item among shoe enthusiasts.
-                </p>
-            </div>
-        </div>
-
     <div className='bg-gray-100 w-8/12'>
         <div className='max-w-2xl block my-0 mx-auto'>
             <div className='flex pt-1.5	gap-x-1	'>
@@ -48,11 +31,7 @@ function MyStore() {
                 <div>Like</div>
             </div>
         </div>
-
     </div>
-    </>
-
-
   )
 }
 
